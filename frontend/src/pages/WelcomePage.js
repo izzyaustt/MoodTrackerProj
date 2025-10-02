86dcba035d189a15d62262e573ca78d05d02700f
sabrina <sabinang1809@gmail.com>
2025-10-02 19:03:28 -0400

    Add a basic welcome page

---
+++ frontend/src/pages/WelcomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WelcomePage.css";
+
+function WelcomePage() {
+    return (
+        <div className="welcome-container">
+            <h1 className="welcome-title">Wellness Journal</h1>
+            <p className="welcome-subtitle">it's the small things that matter</p>
+        </div>
+    );
+}
+
+export default WelcomePage;
