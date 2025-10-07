da0efb2404cb0ec56f65f850df2ca6fc4a9dd01c
nguyenai18 <nguyenimee@gmail.com>
2025-10-07 17:16:00 -0400

    add images and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import WelcomePage from "./pages/WelcomePage";
+import LogInPage from "./pages/LogInPage";
 
 
 function App() {
   return (
       <div className="App">
         <WelcomePage />
+          <LogInPage />
       </div>
   );
 }
