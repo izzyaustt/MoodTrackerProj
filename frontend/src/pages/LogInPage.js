58889ce3eac5b7a3cc0dfdada3cd0ff8ba12edac
nguyenai18 <nguyenimee@gmail.com>
2025-10-07 17:16:00 -0400

    add images and login page

--- frontend/src/pages/LogInPage.js
+++ frontend/src/pages/LogInPage.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./LogInPage.css";
 
+function LogInPage() {
+    return (
+    <div className="logIn-container">
+
+    </div>
+    )
+}
+export default LogInPage;
+
 
 
