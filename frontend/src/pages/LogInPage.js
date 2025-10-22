fe0cf4fc44beb87e07208e9951a4590597198230
nguyenai18 <nguyenimee@gmail.com>
2025-10-22 00:58:29 -0400

    login page finish

--- frontend/src/pages/LogInPage.js
+++ frontend/src/pages/LogInPage.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./LogInPage.css";
 
+import paper from "../Images/LogIn_Paper.png";
+
 function LogInPage() {
     return (
-    <div className="logIn-container">
+        <div className="logIn">
+            <h1>this journal belongs to:</h1>
+            <div className="paperContainer">
+                <img
+                    src={paper}
+                    alt="ripped texture paper"
+                    className="paperInfo"
+                />
+                <div className="paperText">
+                    <p>name______________</p>
+                    <p>email______________</p>
+                    <p>password___________</p>
+                </div>
+            </div>
+        </div>
 
-    </div>
-    )
+    );
 }
+
 export default LogInPage;
-
-
 
